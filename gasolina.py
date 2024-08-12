e7aea0cac47e8bb7f4072be549206200e6c9d2f7
GLK-7 <gabriel.godoitb@gmail.com>
2024-08-12 18:45:24 +0000

    arquivos gasolina.csv, gasolina.py e gasolina.png criados

---
+++ gasolina.py
@@ -0,0 +1,19 @@
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt  # Import necessário para exibir e salvar o gráfico
+
+# Ler o arquivo CSV e passar para um DataFrame
+df_gasolina = pd.read_csv('gasolina.csv')
+df_gasolina.head() 
+
+# Gerar gráfico de linha
+chart = sns.lineplot(data=df_gasolina, x='dia', y='venda')
+chart.set_title('Vendas de Gasolina por Dia')
+chart.set_xlabel('Dia')
+chart.set_ylabel('Venda')  
+
+# Salvar o gráfico como PNG
+plt.savefig('gasolina.png', format='png')
+
+print("Arquivo gasolina.png criado com sucesso.")
