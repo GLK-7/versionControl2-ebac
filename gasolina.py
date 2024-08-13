b1a012457e507d3db61d81198a376b6acbdef2dc
GLK-7 <gabriel.godoitb@gmail.com>
2024-08-13 12:52:26 +0000

    arquivos gasolina.py e gasolina.png modificados

--- gasolina.py
+++ gasolina.py
@@ -9,9 +9,9 @@ df_gasolina.head()
 
 # Gerar gráfico de linha
 chart = sns.lineplot(data=df_gasolina, x='dia', y='venda')
-chart.set_title('Vendas de Gasolina por Dia')
+chart.set_title('Preço da Gasolina por Dia')
 chart.set_xlabel('Dia')
-chart.set_ylabel('Venda')  
+chart.set_ylabel('Preço')  
 
 # Salvar o gráfico como PNG
 plt.savefig('gasolina.png', format='png')
